c051511bd862afbeb03e4c5c1c60b3ad4d5f7302
Nekoi <nekoi275@gmail.com>
2020-03-17 19:09:49 +0200

    added aliens rendering in the game

--- src/components/SpaceInvadersComponent.vue
+++ src/components/SpaceInvadersComponent.vue
@@ -16,71 +16,116 @@ export default {
       isGameOver: false,
       canvasCtx: null,
       spriteFrame: 0,
-      alienSprites: [
-        [
-          {
-            x: 0,
+      config: {
+        aliens: {
+          sprites: [
+            [
+              {
+                x: 0,
+                y: 0,
+                width: 22,
+                height: 16,
+                margin: 0
+              },
+              {
+                x: 0,
+                y: 16,
+                width: 22,
+                height: 16,
+                margin: 0
+              }
+            ],
+            [
+              {
+                x: 22,
+                y: 0,
+                width: 16,
+                height: 16,
+                margin: 4
+              },
+              {
+                x: 22,
+                y: 16,
+                width: 16,
+                height: 16,
+                margin: 4
+              }
+            ],
+            [
+              {
+                x: 38,
+                y: 0,
+                width: 24,
+                height: 16,
+                margin: 0
+              },
+              {
+                x: 38,
+                y: 16,
+                width: 24,
+                height: 16,
+                margin: 0
+              }
+            ]
+          ],
+          rows: 5,
+          columns: 10,
+          types: [1, 0, 0, 2, 2]
+        },
+        tank: {
+          sprite: {
+            x: 62,
             y: 0,
             width: 22,
             height: 16
           },
-          {
-            x: 0,
-            y: 16,
-            width: 22,
-            height: 16
-          }
-        ],
-        [
-          {
-            x: 22,
-            y: 0,
-            width: 16,
-            height: 16
-          },
-          {
-            x: 22,
-            y: 16,
-            width: 16,
-            height: 16
-          }
-        ],
-        [
-          {
-            x: 38,
-            y: 0,
-            width: 24,
-            height: 16
-          },
-          {
-            x: 38,
-            y: 16,
-            width: 24,
-            height: 16
-          }
-        ]
-      ],
-      tankSprite: {
-        x: 62,
-        y: 0,
-        width: 22,
-        height: 16
-      }
+          xOffset: 0
+        }
+      },
+      aliens: []
     };
   },
   methods: {
     start: function() {
       this.buttonShown = false;
-      this.drawSprites();
+      this.init();
+      this.render();
     },
-    drawSprites: function() {
+    render: function() {
       var img = document.createElement("img");
       img.src = spriteImage;
       img.addEventListener("load", () => {
-        let tankXpos = (this.canvasCtx.canvas.width - this.tankSprite.width) / 2;
-        let tankYpos = this.canvasCtx.canvas.height - (30 + this.tankSprite.height);
-        this.drawSprite(img, this.tankSprite, tankXpos, tankYpos);
+        let tankXPos =
+          (this.canvasCtx.canvas.width - this.config.tank.sprite.width) / 2;
+        let tankYPos =
+          this.canvasCtx.canvas.height - (30 + this.config.tank.sprite.height);
+        this.drawSprite(img, this.config.tank.sprite, tankXPos, tankYPos);
+        for (let i = 0; i < this.aliens.length; i++) {
+          let alien = this.aliens[i];
+          this.drawSprite(img, alien.sprite[this.spriteFrame], alien.x, alien.y);
+        }
       });
+    },
+    init: function() {
+      const padding = 30;
+
+      for (let rowIndex = 0; rowIndex < this.config.aliens.rows; rowIndex++) {
+        for (
+          let colIndex = 0;
+          colIndex < this.config.aliens.columns;
+          colIndex++
+        ) {
+          let typeNumber = this.config.aliens.types[rowIndex];
+          let sprite = this.config.aliens.sprites[typeNumber];
+          this.aliens.push({
+            sprite: sprite,
+            x: padding + colIndex * padding + sprite[0].margin,
+            y: padding + rowIndex * padding,
+            w: sprite[0].width,
+            h: sprite[0].height
+          });
+        }
+      }
     },
     drawSprite: function(img, sprite, x, y) {
       this.canvasCtx.drawImage(
@@ -99,8 +144,8 @@ export default {
   mounted: function() {
     var canvas = document.getElementById("game-screen");
     var ctx = canvas.getContext("2d");
-    ctx.canvas.width = 300;
-    ctx.canvas.height = 400;
+    ctx.canvas.width = 600;
+    ctx.canvas.height = 500;
     this.canvasCtx = ctx;
   }
 };
@@ -109,7 +154,7 @@ export default {
 <style scoped>
 .game-container {
   position: relative;
-  width: 300px;
+  width: 600px;
 }
 canvas {
   background-color: #000000;
