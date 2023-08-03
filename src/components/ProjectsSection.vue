8d1352ce7122fd36200270308969ab909efa1617
nekoi <nekoi275@gmail.com>
2023-08-03 15:54:39 +0300

    added sudoku solver + minor fixes

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -1,17 +1,19 @@
 <template>
   <section id="projects">
-    <space-invaders v-show="isDesktop()"/>
-    <a href="http://ave-startups.com/" target="_blank">
-      <div class="projects-row">
-        <div class="projects-col">
-          <img class="projects-img" src="../assets/project_4.png">
+    <div class="wrapper">
+      <space-invaders v-show="isDesktop()" />
+      <sudoku></sudoku>
+      <a href="http://ave-startups.com/" target="_blank">
+        <div class="projects-row">
+          <div class="projects-col">
+            <img class="projects-img" src="../assets/project_4.png">
+            <div class="projects-col-text">
+              <p>AVE Startups website.</p>
+            </div>
+          </div>
         </div>
-        <div class="projects-col-text">
-          <p>AVE Startups website.</p>
-        </div>
-      </div>
-    </a>
-    <sudoku></sudoku>
+      </a>
+    </div>
   </section>
 </template>
 
@@ -22,11 +24,11 @@ import Sudoku from "./SudokuSolverComponent";
 export default {
   name: 'ProjectsSection',
   components: {
-      SpaceInvaders,
-      Sudoku
+    SpaceInvaders,
+    Sudoku
   },
   methods: {
-    isDesktop: function() {
+    isDesktop: function () {
       return window.innerWidth >= 1000;
     }
   }
@@ -36,18 +38,22 @@ export default {
 <style scoped>
 section {
   background-color: var(--dark-font-color);
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  padding-top: 150px;
-  padding-bottom: 150px;
-  gap: 30px;
+  padding-top: 130px;
 }
+
+.wrapper {
+  display: grid;
+  grid-gap: 30px;
+  grid-template-columns: 600px 600px;
+  grid-auto-rows: 520px;
+  justify-content: center;
+}
+
 .projects-img {
   width: 500px;
   height: auto;
 }
+
 .projects-row {
   display: flex;
   flex-direction: column;
@@ -58,24 +64,30 @@ section {
   padding: 10px;
   border: 2px solid var(--light-font-color);
 }
+
 .projects-row:hover {
   border: 2px solid var(--second-main-color);
 }
+
 .projects-col-text {
   color: var(--main-bg-color);
 }
+
 a {
   text-decoration: none;
   height: 100%;
 }
+
 @media (max-width: 845px) {
   section {
     padding-top: 100px;
   }
+
   .projects-img {
     width: 300px;
     height: auto;
   }
+
   a {
     margin: 30px 0px;
   }
--- src/components/SudokuSolverComponent.vue
+++ src/components/SudokuSolverComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="game-container">
-        <table>
+        <table v-show="!isPuzzleSolved">
             <tr>
                 <td><input v-model="puzzle[0][0]"></td>
                 <td><input v-model="puzzle[0][1]"></td>
@@ -101,9 +101,111 @@
                 <td><input v-model="puzzle[8][8]"></td>
             </tr>
         </table>
-        <button @click="validate()">Solve</button>
+        <table v-show="isPuzzleSolved">
+            <tr>
+                <td><span>{{ puzzle[0][0] }}</span></td>
+                <td><span>{{ puzzle[0][1] }}</span></td>
+                <td><span>{{ puzzle[0][2] }}</span></td>
+                <td><span>{{ puzzle[0][3] }}</span></td>
+                <td><span>{{ puzzle[0][4] }}</span></td>
+                <td><span>{{ puzzle[0][5] }}</span></td>
+                <td><span>{{ puzzle[0][6] }}</span></td>
+                <td><span>{{ puzzle[0][7] }}</span></td>
+                <td><span>{{ puzzle[0][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[1][0] }}</span></td>
+                <td><span>{{ puzzle[1][1] }}</span></td>
+                <td><span>{{ puzzle[1][2] }}</span></td>
+                <td><span>{{ puzzle[1][3] }}</span></td>
+                <td><span>{{ puzzle[1][4] }}</span></td>
+                <td><span>{{ puzzle[1][5] }}</span></td>
+                <td><span>{{ puzzle[1][6] }}</span></td>
+                <td><span>{{ puzzle[1][7] }}</span></td>
+                <td><span>{{ puzzle[1][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[2][0] }}</span></td>
+                <td><span>{{ puzzle[2][1] }}</span></td>
+                <td><span>{{ puzzle[2][2] }}</span></td>
+                <td><span>{{ puzzle[2][3] }}</span></td>
+                <td><span>{{ puzzle[2][4] }}</span></td>
+                <td><span>{{ puzzle[2][5] }}</span></td>
+                <td><span>{{ puzzle[2][6] }}</span></td>
+                <td><span>{{ puzzle[2][7] }}</span></td>
+                <td><span>{{ puzzle[2][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[3][0] }}</span></td>
+                <td><span>{{ puzzle[3][1] }}</span></td>
+                <td><span>{{ puzzle[3][2] }}</span></td>
+                <td><span>{{ puzzle[3][3] }}</span></td>
+                <td><span>{{ puzzle[3][4] }}</span></td>
+                <td><span>{{ puzzle[3][5] }}</span></td>
+                <td><span>{{ puzzle[3][6] }}</span></td>
+                <td><span>{{ puzzle[3][7] }}</span></td>
+                <td><span>{{ puzzle[3][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[4][0] }}</span></td>
+                <td><span>{{ puzzle[4][1] }}</span></td>
+                <td><span>{{ puzzle[4][2] }}</span></td>
+                <td><span>{{ puzzle[4][3] }}</span></td>
+                <td><span>{{ puzzle[4][4] }}</span></td>
+                <td><span>{{ puzzle[4][5] }}</span></td>
+                <td><span>{{ puzzle[4][6] }}</span></td>
+                <td><span>{{ puzzle[4][7] }}</span></td>
+                <td><span>{{ puzzle[4][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[5][0] }}</span></td>
+                <td><span>{{ puzzle[5][1] }}</span></td>
+                <td><span>{{ puzzle[5][2] }}</span></td>
+                <td><span>{{ puzzle[5][3] }}</span></td>
+                <td><span>{{ puzzle[5][4] }}</span></td>
+                <td><span>{{ puzzle[5][5] }}</span></td>
+                <td><span>{{ puzzle[5][6] }}</span></td>
+                <td><span>{{ puzzle[5][7] }}</span></td>
+                <td><span>{{ puzzle[5][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[6][0] }}</span></td>
+                <td><span>{{ puzzle[6][1] }}</span></td>
+                <td><span>{{ puzzle[6][2] }}</span></td>
+                <td><span>{{ puzzle[6][3] }}</span></td>
+                <td><span>{{ puzzle[6][4] }}</span></td>
+                <td><span>{{ puzzle[6][5] }}</span></td>
+                <td><span>{{ puzzle[6][6] }}</span></td>
+                <td><span>{{ puzzle[6][7] }}</span></td>
+                <td><span>{{ puzzle[6][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[7][0] }}</span></td>
+                <td><span>{{ puzzle[7][1] }}</span></td>
+                <td><span>{{ puzzle[7][2] }}</span></td>
+                <td><span>{{ puzzle[7][3] }}</span></td>
+                <td><span>{{ puzzle[7][4] }}</span></td>
+                <td><span>{{ puzzle[7][5] }}</span></td>
+                <td><span>{{ puzzle[7][6] }}</span></td>
+                <td><span>{{ puzzle[7][7] }}</span></td>
+                <td><span>{{ puzzle[7][8] }}</span></td>
+            </tr>
+            <tr>
+                <td><span>{{ puzzle[8][0] }}</span></td>
+                <td><span>{{ puzzle[8][1] }}</span></td>
+                <td><span>{{ puzzle[8][2] }}</span></td>
+                <td><span>{{ puzzle[8][3] }}</span></td>
+                <td><span>{{ puzzle[8][4] }}</span></td>
+                <td><span>{{ puzzle[8][5] }}</span></td>
+                <td><span>{{ puzzle[8][6] }}</span></td>
+                <td><span>{{ puzzle[8][7] }}</span></td>
+                <td><span>{{ puzzle[8][8] }}</span></td>
+            </tr>
+        </table>
+        <button v-show="!isPuzzleSolved && isPuzzleValid" @click="validatePuzzle()">Solve</button>
+        <button v-show="isPuzzleSolved || !isPuzzleValid" @click="clearPuzzle()">Try again</button>
         <p v-show="!isPuzzleValid">Fill some known numbers of sudoku. Valid numbers are 1-9.</p>
-        <p>Solves a <a href="https://en.wikipedia.org/wiki/Sudoku">Sudoku</a> by the provided "clues".</p>
+        <p v-show="isPuzzleValid">Solves a <a href="https://en.wikipedia.org/wiki/Sudoku" target="_blank">Sudoku</a> by the provided "clues"</p>
     </div>
 </template>
   
@@ -112,7 +214,43 @@ export default {
     name: 'Sudoku',
     data: function () {
         return {
-            puzzle: [
+            /* puzzle: [
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null]
+            ], */
+            puzzle: [[null,null,"3",null,null,"1",null,null,null],["7",null,null,"8",null,null,"5","2",null],["8",null,"4",null,null,null,"6",null,"3"],[null,null,"7","3","2",null,null,null,null],["5",null,null,null,null,null,null,null,"2"],[null,null,null,null,"1","7","4","3",null],[null,"6","5",null,null,"4",null,null,null],[null,"2","9","1","8","5","3",null,"7"],[null,"7","8","6","3",null,"9","5","4"]],
+            isPuzzleValid: true,
+            isPuzzleSolved: false
+        };
+    },
+    methods: {
+        validatePuzzle: function () {
+            outer_loop:
+            for (let i = 0; i < this.puzzle.length; i++) {
+                for (let j = 0; j < this.puzzle[i].length; j++) {
+                    if (![null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(this.puzzle[i][j]))) {
+                        this.isPuzzleValid = false;
+                        break outer_loop;
+                    } else {
+                        this.isPuzzleValid = true;
+                    }
+                }
+            }
+            if (this.isPuzzleValid) {
+                this.preparePuzzle();
+                this.solvePuzzle();
+                this.isPuzzleSolved = true;
+            }
+        },
+        clearPuzzle: function () {
+            this.puzzle = [
                 [null, null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null, null],
@@ -123,26 +261,64 @@ export default {
                 [null, null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null, null]
             ],
-            isPuzzleValid: true
-        };
-    },
-    methods: {
-        validate: function () {
-            outer_loop:
+                this.isPuzzleSolved = false;
+                this.isPuzzleValid = true;
+        },
+        preparePuzzle: function () {
             for (let i = 0; i < this.puzzle.length; i++) {
                 for (let j = 0; j < this.puzzle[i].length; j++) {
-                    if (![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(this.puzzle[i][j]))) {
-                        this.isPuzzleValid = false;
-                        break outer_loop;
-                    } else {
-                        this.isPuzzleValid = true;
+                    this.puzzle[i][j] = Number(this.puzzle[i][j])
+                }
+            }
+        },
+        isNumberValid: function (puzzle, row, col, val) {
+            for (let i = 0; i < 9; i++) {
+                if (puzzle[row][i] === val) {
+                    return false;
+                }
+            }
+            for (let i = 0; i < 9; i++) {
+                if (puzzle[i][col] === val) {
+                    return false;
+                }
+            }
+            let squareRow = Math.floor(row / 3) * 3;
+            let squareCol = Math.floor(col / 3) * 3;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (puzzle[squareRow + i][squareCol + j] === val)
+                        return false;
+                }
+            }
+            return true;
+        },
+        findEmptyCell: function (puzzle) {
+            for (let row = 0; row < 9; row++) {
+                for (let col = 0; col < 9; col++) {
+                    if (puzzle[row][col] === 0 || puzzle[row][col] === null) {
+                        return [row, col]
                     }
                 }
             }
-            if (this.isPuzzleValid) this.solvePuzzle();
         },
         solvePuzzle: function () {
-            console.log('solved')
+            let empty = this.findEmptyCell(this.puzzle);
+            if (empty) {
+                let [row, col] = empty;
+                for (let num = 1; num <= 9; num++) {
+                    if (this.isNumberValid(this.puzzle, row, col, num)) {
+                        this.puzzle[row][col] = num;
+                        let result = this.solvePuzzle(this.puzzle);
+                        if (result) {
+                            return result;
+                        } else {
+                            this.puzzle[row][col] = 0;
+                        }
+                    }
+                }
+                return;
+            }
+            return this.puzzle;
         }
     }
 };
@@ -153,7 +329,7 @@ export default {
     position: relative;
     background-color: var(--main-bg-color);
     width: 600px;
-    height: 650px;
+    height: 500px;
 }
 
 p {
@@ -161,18 +337,19 @@ p {
     color: var(--dark-font-color);
     border: 2px solid var(--second-main-color);
     text-align: center;
-    margin: 15px;
+    margin: 10px;
 }
 
 table {
-    margin: 15px auto;
+    margin: 10px auto;
     border-collapse: collapse;
 }
 
 td {
-    padding: 10px;
-    width: 20px;
-    height: 20px;
+    padding: 5px;
+    width: 30px;
+    height: 30px;
+    text-align: center;
     border-left: 2px solid var(--header-bg-color);
     border-right: 2px solid var(--header-bg-color);
     border-bottom: 2px solid var(--header-bg-color);
@@ -196,11 +373,14 @@ tr:nth-child(3n) td {
 
 input {
     border: none;
-    width: 15px;
     background-color: transparent;
     text-align: center;
     outline: none;
-    font-size: 18px;
+    width: 15px;
+}
+
+span {
+    padding: 5px;
 }
 
 button {
@@ -208,10 +388,9 @@ button {
     border: none;
     background-color: var(--light-font-color);
     color: var(--dark-font-color);
-    padding: 15px;
+    padding: 10px;
     border-radius: 20px;
     cursor: pointer;
-    font-size: 20px;
     letter-spacing: 2px;
     text-align: center;
     display: block;
@@ -222,4 +401,3 @@ button:hover {
     background-color: var(--dark-font-color);
 }
 </style>
-  
